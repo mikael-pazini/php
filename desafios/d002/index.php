3be90812b2dd7f2287951c8d81e61fc5d23d4828
Mikael Pazini <125323236+mikael-pazini@users.noreply.github.com>
2024-10-09 23:18:12 -0300

    Update index.php

--- desafios/d002/index.php
+++ desafios/d002/index.php
@@ -11,6 +11,12 @@
     <main>
         
         <?php
+
+        // rand() = 1951 - Linear Congrential Generator
+        // mt_rand() = 1997 - Mersenne Twister
+        // A partir do PHP 7.1, rand() é um simples apontamento para mt_rand()
+        // random_int() gera números aleatórios criptograficamente seguros
+        
             $numAle = 0;
             function gerarNumAle() {
                 return rand(1, 100);
