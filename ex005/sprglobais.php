e1edc3bde88411fd6d2f91998d2b5644803341b4
Mikael Pazini <125323236+mikael-pazini@users.noreply.github.com>
2024-10-11 08:50:47 -0300

    update

---
+++ ex005/sprglobais.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Super Globais em PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <pre>
+            <?php 
+                echo"<h1>Superglobal GET</h1>";
+                var_dump($_GET)
+            ?>
+        </pre>
+    </main>
+</body>
+</html>
