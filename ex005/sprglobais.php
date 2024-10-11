0c3bbcf59598557b0caee425a9f544c1dccda5b8
Mikael Pazini <125323236+mikael-pazini@users.noreply.github.com>
2024-10-11 09:18:20 -0300

    Update sprglobais.php

--- ex005/sprglobais.php
+++ ex005/sprglobais.php
@@ -9,7 +9,12 @@
 <body>
     <main>
         <pre>
-            <?php 
+            <?php
+                setcookie("dia-da-semana","SEXTA", time() +3600,);
+
+                session_start();
+                $_SESSION["teste"] = "FUNCIONOU.";
+
                 echo"<h1>Superglobal GET</h1>";
                 var_dump($_GET);
 
@@ -18,6 +23,24 @@
 
                 echo"<h1>Superglobal REQUEST</h1>";
                 var_dump($_REQUEST);
+
+                echo"<h1>Superglobal COOKIE</h1>";
+                var_dump($_COOKIE);
+
+                echo"<h1>Superglobal SESSION</h1>";
+                var_dump($_SESSION);
+
+                echo"<h1>Superglobal ENV</h1>";
+                var_dump($_ENV);
+                //foreach (getenv() as $key => $value) {
+                //    echo "<br> $key -> $value";
+                //}
+
+                echo"<h1>Superglobal SERVER</h1>";
+                var_dump($_SERVER);
+
+                echo"<h1>Superglobal GLOBALS</h1>";
+                var_dump($GLOBALS);
             ?>
         </pre>
     </main>
