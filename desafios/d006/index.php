084e637b369d567886ac1575e28607a3f8632a8e
Mikael Pazini <125323236+mikael-pazini@users.noreply.github.com>
2024-10-13 12:59:52 -0300

    Update index.php

--- desafios/d006/index.php
+++ desafios/d006/index.php
@@ -9,7 +9,7 @@
 <body>
     <?php 
         // Capturando os dados do Formulário Retroalimentado
-        $divid = $_GET['dividendo']?? 1;
+        $divid = $_GET['dividendo']?? 0;
         $divis = $_GET['divisor']?? 1;
 
     ?>
@@ -17,9 +17,9 @@
         <h1>Anatomia de uma divisão</h1>
         <form action="<?= $_SERVER['PHP_SELF']?>" method="get">
             <label for="dividendo">Dividendo</label>
-            <input type="number" name="dividendo" id="dividendo" required value="<?=$divid?>">
+            <input type="number" name="dividendo" id="dividendo" min="0" required value="<?=$divid?>">
             <label for="divisor">Divisor</label>
-            <input type="number" name="divisor" id="divisor" required value="<?=$divis?>" >
+            <input type="number" name="divisor" id="divisor" min="1" required value="<?=$divis?>" >
             <input type="submit" value="Dividir">
         </form>
     </main>
@@ -28,13 +28,22 @@
         <h2>Resultado da Operação</h2>
         <?php 
             $div = $divid / $divis;
-            echo "A divisão entre os valores ".$divid." e ".$divis." é <strong>igua a $div</strong>.<br>";
+            echo "A divisão entre os valores ".$divid." e ".$divis." é <strong>igua a ".number_format($div,3,",",".")."</strong>.<br>";
             echo "O dividendo é <strong>$divid</strong>.<br>";
             echo "O divisor é <strong>$divis</strong>.<br>";
-            echo "A divisão inteira é <strong>".(int)$div."</strong>.<br>";
+            echo "O quociente é <strong>".(int)$div."</strong>.<br>";
             echo "O resto da divisão é <strong>".($divid % $divis)."</strong>.<br>";
         ?>
-        
+        <table class="divisao">
+            <tr>
+                <td><?=$divid?></td>
+                <td><?=$divis?></td>
+            </tr>
+            <tr>
+                <td><?=($divid % $divis)?></td>
+                <td><?=(int)$div?></td>
+            </tr>
+        </table>
     </section>
 </body>
 </html>
